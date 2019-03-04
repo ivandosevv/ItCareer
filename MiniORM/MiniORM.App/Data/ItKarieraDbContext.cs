f7275135323868aaccc8ac50c846a6895c777bed
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2019-03-04 21:58:58 +0200

    Add files via upload

---
+++ MiniORM/MiniORM.App/Data/ItKarieraDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MiniORM.App.Data
+{
+    using Entities;
+
+    public class ItKarieraDbContext : DbContext
+    {
+        public ItKarieraDbContext(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+
+        public DbSet<Employee> Employees
+        {
+            get;
+        }
+
+        public DbSet<Project> Projects
+        {
+            get;
+        }
+
+        public DbSet<Department> Departments
+        {
+            get;
+        }
+
+        public DbSet<EmployeeProject> EmployeesProjects
+        {
+            get;
+        }
+    }
+}
